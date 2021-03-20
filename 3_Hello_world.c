e3caa2956a936e4837fe146b4684451ced848d0b
Bhanu Verma <2020a1r163@mietjammu.in>
2021-03-20 15:26:53 +0530

    Printing Hello world

---
+++ 3_Hello_world.c
@@ -0,0 +1,11 @@
+//PRINTING "HELLO WORLD"
+//Writted by -- BHANU VERMA
+
+#include<stdio.h> //Pre-Process directive to include standard input output header file.
+int main(){ // Main function body starting..
+
+	//using printf function of standard library to print Hello world
+	printf("Hello World"); 
+	return 0;
+
+}
