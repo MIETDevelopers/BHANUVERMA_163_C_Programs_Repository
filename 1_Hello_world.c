440426d1937bc022db359085bfdca10bd92c53dc
Bhanu Verma <2020a1r163@mietjammu.in>
2021-03-20 14:13:38 +0530

    Printing Hello world

--- 1_Hello_world.c
+++ 1_Hello_world.c
@@ -3,9 +3,9 @@
 
 #include<stdio.h> //Pre-Process directive to include standard input output header file.
 int main(){ // Main function body starting..
-	
+
 	//using printf function of standard library to print Hello world
-	printf("Hello World"); .
+	printf("Hello World"); 
 	return 0;
 
 }
