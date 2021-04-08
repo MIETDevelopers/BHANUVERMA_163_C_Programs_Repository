d987778eaee5e37b710282649ec332e02ebb334c
Bhanu Verma <2020a1r163@mietjammu.in>
2021-04-08 08:51:51 +0530

    Area of Circle,Square and Rectangle based on User's choice

--- 0009_AreaUserChoice.c
+++ 0009_AreaUserChoice.c
@@ -6,7 +6,7 @@
 #include <stdio.h>  //Pre-process directive to include standard input output functions header file.
 int main () //Main function body starting.
 {
-      int choice,r,l,w,s; //variable declaration of integer data type.
+      int choice,r,l,b,s; //variable declaration of integer data type.
       float area; // variable declaration of float data type
       printf("Input 1 for area of circle\n");
       printf("Input 2 for area of rectangle\n");
@@ -25,14 +25,14 @@ int main () //Main function body starting.
             //Area of Rectangle
             case 2:
                   printf("Input length and width of the rectangle : "); 
-                  scanf("%d%d",&l,&w);//taking length and breadth as a input from the user.
-                  area=l*w; //formula of area of rectangle.
+                  scanf("%d%d",&l,&b);//taking length and breadth as a input from the user.
+                  area=l*b; //formula of area of rectangle.
                   break;
             //Area of Square
             case 3:
                   printf("Input the side of the square : ");
                   scanf("%d",&s);//taking side of square as a input from the user.
-                  area=.5*s*s; //formula of area of square.
+                  area=s*s; //formula of area of square.
                   break;
           }
           printf("The area is : %f\n",area); // Printing the area according to the user's choice
